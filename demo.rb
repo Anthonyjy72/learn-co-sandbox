3c6fb1c813e15e14df4e20a9760c344b7f3a38ae
Anthony Ybarra <anthony_jy72@yahoo.com>
2020-07-29 23:29:06 +0000

    Automatically backed up by Learn

---
+++ demo.rb
@@ -0,0 +1,13 @@
+name = "The Mad Hatter"
+
+if name == "Alice"
+  puts "Hello Alice!"
+elsif name == "White Rabbit"
+  puts "Dont be late, White Rabbit"
+elsif name == "The Mad Hatter"
+  puts "Welcome to the tea party, Mad Hatter"
+elsif name == "The Queen of Hearts"
+  puts "Please dont chop off my head!"
+else
+  puts "Who are you?"
+end
